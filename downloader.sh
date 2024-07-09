a9da1b5939c7526557238b83a111b2f1c96d70f7
Aaron <80927625+aaaguayo@users.noreply.github.com>
2024-07-09 08:02:35 +0200

    Update downloader.sh
    English translation

--- downloader.sh
+++ downloader.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Pedir al usuario que ingrese la ID del video
-echo "Por favor, ingresa la ID del video de Wistia:"
+# Prompt the user to enter the video ID
+echo "Please enter the Wistia video ID:"
 read video_id
 
-# Comando para descargar el video usando la ID proporcionada
+# Command to download the video using the provided ID
 wget -qO- "http://fast.wistia.net/embed/iframe/$video_id" \
 | sed -n -e 's#^\s*W\.iframeInit(\(.*\), {[^}]*});\s*$#\1#p' \
 | jq -r '."assets"[0]."url" | sub("\\.bin$"; ".mp4")' \
 | xargs curl -O
 
-echo "Descarga completada."
+echo "Download completed."
